a5e91525f385d6aae3acda3717798a9e54471e4c
Vepashka94 <51997203+Vepashka94@users.noreply.github.com>
2026-06-03 17:51:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     showJobs(jobs);
 
     document.getElementById("searchInput").addEventListener("input", (e) => {
+
         let val = e.target.value.toLowerCase();
 
         let filtered = jobs.filter(j =>
@@ -11,27 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
         showJobs(filtered);
     });
+
 });
 
 function showJobs(data) {
+
     const container = document.getElementById("jobs-container");
 
     container.innerHTML = "";
 
     data.forEach(job => {
+
         container.innerHTML += `
         <div class="job-card">
 
             <img src="${job.image}" />
 
             <div class="job-content">
-                <div class="job-title">${job.title}</div>
-                <div>📍 ${job.city}</div>
-                <div>💰 ${job.salary}</div>
-                <div>${job.description}</div>
 
-                <a href="https://wa.me/${job.whatsapp}">WhatsApp</a>
+                <h3>${job.title}</h3>
+                <p>📍 ${job.city}</p>
+                <p>💰 ${job.salary}</p>
+                <p>${job.description}</p>
+
+                <a href="https://wa.me/${job.whatsapp}" target="_blank">WhatsApp</a>
                 <a href="tel:${job.phone}">Ara</a>
+
             </div>
 
         </div>
